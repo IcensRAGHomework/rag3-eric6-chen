b6cd2edb7637c687a13a0fcd5696010a92a86428
Eric6_Chen <eric6_chen@asus.com>
2025-03-10 15:23:12 +0800

    setup environment

--- model_configurations.py
+++ model_configurations.py
@@ -1,17 +1,19 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
+load_dotenv(override=True)
 
 configurations = {
     "text-embedding-ada-002": {
-        "api_base": os.getenv('AZURE_OPENAI_EMBEDDING_ENDPOINT'),
-        "api_key": os.getenv('AZURE_OPENAI_EMBEDDING_KEY'),
-        "deployment_name": os.getenv('AZURE_OPENAI_DEPLOYMENT_EMBEDDING'),
-        "api_version": os.getenv('AZURE_OPENAI_VERSION'),
-        "model": os.getenv('AZURE_OPENAI_DEPLOYMENT_EMBEDDING_MODEL_NAME'),
-        "openai_type": os.getenv('AZURE_OPENAI_TYPE')
+        "api_base": os.getenv("AZURE_OPENAI_EMBEDDING_ENDPOINT"),
+        "api_key": os.getenv("AZURE_OPENAI_EMBEDDING_KEY"),
+        "deployment_name": os.getenv("AZURE_OPENAI_DEPLOYMENT_EMBEDDING"),
+        "api_version": os.getenv("AZURE_OPENAI_VERSION"),
+        "model": os.getenv("AZURE_OPENAI_DEPLOYMENT_EMBEDDING_MODEL_NAME"),
+        "openai_type": os.getenv("AZURE_OPENAI_TYPE"),
     }
 }
+
 
 def get_model_configuration(model_version):
     return configurations.get(model_version)
--- student_assignment.py
+++ student_assignment.py
@@ -6,33 +6,40 @@ from chromadb.utils import embedding_functions
 
 from model_configurations import get_model_configuration
 
-gpt_emb_version = 'text-embedding-ada-002'
+gpt_emb_version = "text-embedding-ada-002"
 gpt_emb_config = get_model_configuration(gpt_emb_version)
 
 dbpath = "./"
 
+
 def generate_hw01():
     pass
-    
+
+
 def generate_hw02(question, city, store_type, start_date, end_date):
     pass
-    
+
+
 def generate_hw03(question, store_name, new_store_name, city, store_type):
     pass
-    
+
+
 def demo(question):
     chroma_client = chromadb.PersistentClient(path=dbpath)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-        api_key = gpt_emb_config['api_key'],
-        api_base = gpt_emb_config['api_base'],
-        api_type = gpt_emb_config['openai_type'],
-        api_version = gpt_emb_config['api_version'],
-        deployment_id = gpt_emb_config['deployment_name']
+        api_key=gpt_emb_config["api_key"],
+        api_base=gpt_emb_config["api_base"],
+        api_type=gpt_emb_config["openai_type"],
+        api_version=gpt_emb_config["api_version"],
+        deployment_id=gpt_emb_config["deployment_name"],
     )
     collection = chroma_client.get_or_create_collection(
-        name="TRAVEL",
-        metadata={"hnsw:space": "cosine"},
-        embedding_function=openai_ef
+        name="TRAVEL", metadata={"hnsw:space": "cosine"}, embedding_function=openai_ef
     )
-    
+
     return collection
+
+
+if __name__ == "__main__":
+    # print("test")
+    print(demo("test"))
